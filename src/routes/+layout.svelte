1918c3268c08bcb347940ec37900d49ba4b7bb9b
Rowan Carlsen <rowan.carlsen@gmail.com>
2023-01-25 22:40:34 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+    let headerPct = 51;
+    function slideHeader(e) {
+        headerPct = (e.clientX / this.offsetWidth) * 100;
+    }
+</script>
+
+<header
+    style="background-image: linear-gradient(to right, var(--color-primary) {headerPct}%, var(--color-opposite) {headerPct}%);"
+    on:mousemove={slideHeader}>
+    <div>WEB</div>
+    <span id="header-edu">EDU</span>
+    <span id="header-dev">DEV</span>
+</header>
+<nav class="nav-bar">
+    <a href="#about" class="nav-link">About Me</a>
+    <a href="#portfolio" class="nav-link">Portfolio</a>
+    <a href="#resume" class="nav-link">Resume</a>
+    <a href="#contact" class="nav-link">Contact</a>
+</nav>
+<slot />
+
+<style>
+    .nav-bar {
+        display: flex;
+        justify-content: center;
+        gap: 1em;
+    }
+    header {
+        font-size: 6vw;
+        grid-column: span 2;
+        height: 15vw;
+        position: relative;
+        text-align: center;
+        user-select: none;
+    }
+    header div {
+        mix-blend-mode: difference;
+        color: orange;
+    }
+    header span {
+        position: absolute;
+        bottom: 0;
+        color: white;
+    }
+    #header-dev {
+        right: 55%;
+        mix-blend-mode: difference;
+    }
+    #header-edu {
+        left: 55%;
+        mix-blend-mode: difference;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Rowan Carlsen: Educational Developer and Tech Liaison</title>
+</svelte:head>
+
+<main>test</main>
+
+<style>
+</style>
