64fe9a1362d1dedd11466114858ba6f50342f9ac
Rowan Carlsen <rowan.carlsen@gmail.com>
2023-05-29 10:14:13 -0400

    logos for contact, added GitHub

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -2,17 +2,24 @@
     <h2 id="contact">Contact</h2>
 
     <ul>
-        <li>
+        <li style="--customIcon: url('./logos/github-mark.svg')">
             <a
+                href="https://github.com/rowan-carlsen"
                 target="_blank"
-                rel="noreferrer"
-                href="https://fosstodon.org/@rowan_c">Mastodon</a>
+                rel="noreferrer">GitHub</a>
         </li>
-        <li>
+
+        <li style="--customIcon: url('./logos/In-White-72.png')">
             <a
                 href="https://www.linkedin.com/in/rowan-carlsen-464a35195"
                 target="_blank"
                 rel="noreferrer">LinkedIn</a>
+        </li>
+        <li style="--customIcon: url('./logos/mastodon-logo.svg')">
+            <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://fosstodon.org/@rowan_c">Mastodon</a>
         </li>
     </ul>
 </div>
@@ -24,15 +31,30 @@
     ul {
         padding-left: 0;
         list-style-position: inside;
+        list-style-type: none;
         margin: 0;
     }
     div {
         background-color: var(--color-analogous2);
-        padding: 1em 3em;
+        padding: 1em 2em;
         color: #ced3fd;
         border-radius: 0.5em 0 0 0;
     }
     a {
         color: inherit;
+        display: flex;
+        align-items: center;
+    }
+    li {
+        margin-bottom: 0.5em;
+    }
+    a::before {
+        content: '';
+        background-image: var(--customIcon);
+        background-size: 100%;
+        width: 2em;
+        height: 2em;
+        display: inline-block;
+        margin-right: 0.5em;
     }
 </style>
